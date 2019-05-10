9987735e0999cd6b29e8e30e57794c7e97fffed4
ruby -v <Jbssantias@hotmail.com>
2019-05-10 21:09:07 +1000

    added readme file

---
+++ Lambda_function.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(id)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('AWS Lambda says hello! Encrypt this: Fraq gur synt{FQWSTO7934DO} gb pbagnpg@dhgjuvgrungf.pyho jvgu lbhe anzr naq fghqrag ahzore')
+    }
